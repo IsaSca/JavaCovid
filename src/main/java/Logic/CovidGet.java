1b605b3c9e9886807455bb50de4c709ad76669c3
Isaac Scarisbrick <isasca@hotmail.co.uk>
2020-06-09 16:05:28 +0100

    Reduced API Calls.

--- src/main/java/Logic/CovidGet.java
+++ src/main/java/Logic/CovidGet.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -15,19 +16,32 @@ public class CovidGet {
   private final OkHttpClient client = new OkHttpClient();
   private final ObjectMapper mapper = new ObjectMapper();
   
-  public LinkedHashMap<String, Object> getInfo(String country) throws JsonProcessingException {
-    String allInfo = getAllInfo();
-    if(allInfo.contains("maximum request limit")) {
+  public LinkedHashMap<String, Object> getInfo(String allInfo, String country) throws JsonProcessingException {
+    if (allInfo.contains("maximum request limit")) {
       System.out.println("Maximum API calls reached. Try again in 1 minute.");
       return null;
     }
     HashMap<String, Object> mapped = mapper.readValue(allInfo, HashMap.class);
     ArrayList<LinkedHashMap<String, Object>> countries = (ArrayList<LinkedHashMap<String, Object>>) mapped.get("Countries");
-    for (LinkedHashMap<String, Object> l: countries) {
-      if(l.containsValue(country)) {
-        return l;
+    
+    if (country.equals("help")) {
+      System.out.println("Please enter an available country code.\n" +
+        "Available codes are:\n");
+      for (LinkedHashMap<String, Object> l : countries) {
+        System.out.println(l.get("Country").toString() + ": "
+          + l.get("CountryCode").toString());
       }
+    } else if (country.equals("")) {
+      System.out.println("Please enter a country code. Type covid help to see them.");
+    } else {
+      for (LinkedHashMap<String, Object> l : countries) {
+        if (l.containsValue(country)) {
+          return l;
+        }
+      }
+      System.out.println("No data found for that country code.");
     }
+    
     return null;
   }
   
@@ -38,7 +52,7 @@ public class CovidGet {
     try (Response response = client.newCall(request).execute()) {
       return Objects.requireNonNull(response.body()).string();
     } catch (Exception e) {
-      return("Error in connection.\nYou may have exceeded API usage. Try again in 1 minute.");
+      return ("Error in connection.\nYou may have exceeded API usage. Try again in 1 minute.");
     }
   }
   
--- src/test/java/tests/ResultDisplayTest.java
+++ src/test/java/tests/ResultDisplayTest.java
@@ -1,17 +1,27 @@
 package tests;
 
+import Logic.CovidGet;
 import Logic.ResultDisplay;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedHashMap;
+
 class ResultDisplayTest {
   ResultDisplay display;
+  ResultDisplay displayHelp;
+  ResultDisplay displayWrong;
+  CovidGet covidGet = new CovidGet();
+  String results = covidGet.getAllInfo();
   
   @BeforeEach
   void setUp() throws JsonProcessingException {
-    display = new ResultDisplay("united-states");
+
+    display = new ResultDisplay(results, "GB");
+    displayHelp = new ResultDisplay(results, "help");
+    displayWrong = new ResultDisplay(results, "wrong");
   }
   
   @AfterEach
@@ -20,9 +30,6 @@ class ResultDisplayTest {
   
   @Test
   void testToString() {
-    if(display.getCountryName().equals("")) {
-      return;
-    }
     System.out.println(display.toString());
   }
 }
